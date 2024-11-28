2becebde2f80f851a3434230c5028051d6b2a30d
TH3 EL1T3 GH0ST <th3el1t3gh0st@gmail.com>
2024-11-29 03:04:29 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+os.system('clear')
+
+print("""     \033[1;104mTHIS TOOLS IS EDUCATIONAL PURPOSES ONLY\033[0;37m     
+\033[1;36m           WELCOME TO GHOST WORLD🌎💀
+\033[31;1m╔══════════════════════════════════════════════╗
+\033[31;1m║\033[32;1m  ██████╗ ██╗  ██╗ ██████╗ ███████╗████████╗\033[31;1m  ║
+\033[31;1m║\033[32;1m ██╔════╝ ██║  ██║██╔═══██╗██╔════╝╚══██╔══╝\033[31;1m  ║
+\033[31;1m║\033[32;1m ██║  ███╗███████║██║   ██║███████╗   ██║\033[31;1m     ║
+\033[31;1m║\033[32;1m ██║   ██║██╔══██║██║   ██║╚════██║   ██║\033[31;1m     ║
+\033[31;1m║\033[32;1m  ██████╔╝██║  ██║╚██████╔╝███████║   ██║\033[1;37mV.6.9\033[31;1m║
+\033[31;1m║\033[32;1m  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝\033[31;1m     ║
+\033[31;1m║\033[32;1m   \033[1;36m[\033[1;34m!\033[1;36m]  \033[1;35mTOOLS CREATE BY TH3 EL1T3 GH0ST \033[31;1m      ║
+\033[31;1m╚══════════════════════════════════════════════╝
+\033[1;36m  Tools Name : GHOST DDOS ATTACK TOOLS
+\033[1;37m             TYPE :  GIFT
+ \033[1;33m# Telegram: https://t.me/TH3EL1T3GH0STOFFICIAL
+\033[1;34m           Coding By: @TH3EL1T3GH0ST
+\033[1;35m═══════════════════════════════════════════════\033[32;1m  """)
+
+
+os.system('pip3 install google')
+os.system('pip3 install colorama')
+os.system('apt install wget')
+os.system('pip3 install wget')
+os.system('pip3 install requests')
+os.system('pip3 install sockets')
+os.system('pip3 install pythonping')
+os.system('pip3 install os-sys')
+os.system('pip3 install times')
+os.system('pip3 install sys')
+os.system('pip3 install ipaddress')
+os.system('pip3 install pyexiftool')
+os.system('pip3 install beautifulsoup4')
+os.system('pip3 install phonenumbers')
+os.system('pip3 install faker')
+os.system('pip3 install urllib3')
+os.system('pip3 install regex')
+os.system('pip3 install argparse')
+os.system('pip3 install random2')
+os.system('pip3 install openai')
+os.system('pip3 install importlib')
+os.system('pip3 install scapy')
+os.system('pip3 install cloudscraper')
